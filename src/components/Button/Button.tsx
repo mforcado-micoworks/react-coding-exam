76ed5cc147e860af4c8b763af632e1478f574069
Marjo Forcado <m.forcado@micoworks.jp>
2024-04-04 10:20:41 +0800

    Add type

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -9,7 +9,7 @@ export const Button = (props: TProps) => {
   const { children, ...rest } = props;
 
   return (
-    <button {...rest} className="border rounded p-3">
+    <button {...rest} type="button" className="border rounded p-3">
       {children}
     </button>
   );
